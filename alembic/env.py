44d6c6fbcb802fc2d03a58bac55fd8f7c9815b49
Enzo Maneira <enzo@maneira.com>
2026-06-27 18:25:02 -0300

    fixing search by number2

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,7 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import pool
 
 from app.core.config import settings
 from app.core.database import Base
@@ -37,11 +37,9 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    from sqlalchemy import create_engine
+
+    connectable = create_engine(settings.database_url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
